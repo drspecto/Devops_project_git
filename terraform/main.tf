2ec7ce877f510ea1880ccb480d7f9a2cf726149c
Sarthak Sharma <sarthaksharma@Sarthaks-MacBook-Air.local>
2025-08-03 11:46:05 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ resource "aws_instance" "web"{
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = aws_key_pair.deployer.key_name
-    vpc_security_group_ids = [aws_security_group.allow_ssh_http_4.id] 
+    vpc_security_group_ids = [aws_security_group.allow_ssh_http_5.id] 
     user_data = file("${path.module}/user_data.sh")
 
 
